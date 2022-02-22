b344afc6dd67d2e81905f409f4705be3e669f817
alfygitau <alfygitau@gmail.com>
2022-02-22 12:58:06 +0300

    added edit modal

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,10 +1,20 @@
-import React from "react";
 import "./postCard.css";
 import { RiDeleteBin2Line } from "react-icons/ri";
 import { GrUpdate } from "react-icons/gr";
 import { Link } from "react-router-dom";
+import { Modal, Button, ButtonToolbar, Paragrah } from "rsuite";
+import { useState } from "react";
 
 const PostCard = ({ id, author, title, description, handleDelete }) => {
+
+    const [open, setOpen] = useState(false);
+    const [size, setSize] = useState();
+    const handleOpen = value => {
+      setSize(value);
+      setOpen(true);
+    };
+    const handleClose = () => setOpen(false);
+
   return (
     <div className="card-container">
       <Link to={`/posts/${id}`}>
@@ -25,9 +35,75 @@ const PostCard = ({ id, author, title, description, handleDelete }) => {
         >
           delete | <RiDeleteBin2Line />
         </button>
-        <button type="submit" className="btn btn-outline-warning">
-          update | <GrUpdate />
-        </button>
+        <div className="modal-container">
+        <ButtonToolbar>
+          <Button size="md" onClick={() => handleOpen("md")}>
+            update | <GrUpdate />
+          </Button>
+        </ButtonToolbar>
+        <Modal size={size} open={open} onClose={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Edit the post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Author
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={author}
+          />
+          <div id="emailHelp" className="form-text">
+            Author is required
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={title}
+          />
+          <div id="emailHelp" className="form-text">
+            Title is required
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={description}
+          />
+          <div id="emailHelp" className="form-text">
+            description is required
+          </div>
+        </div>
+      </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose} appearance="primary">
+            Edit
+          </Button>
+          <Button onClick={handleClose} appearance="subtle">
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
       </div>
     </div>
   );
